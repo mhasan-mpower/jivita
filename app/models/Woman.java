1156a035fca8290b44fa32cf1edc6bd5137d9f02
Rifat Nabi <to.rifat@gmail.com>
2011-08-03 16:08:28 +0600

    Women model added

---
+++ app/models/Woman.java
@@ -0,0 +1,28 @@
+package models;
+
+import play.*;
+import play.db.jpa.*;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Entity
+public class Woman extends Model {
+	public String name;
+	public String husbandName;
+	public Long sectorId;
+	// House Hold ID
+	public Long hhId;
+	public String mauzaName;
+	public Long tiPinCd;
+	public Long urinePositiveWeek;
+	public Date lmp;
+	
+	public Woman(String name, String husbandName, Long sectorId, Long hhId){
+		this.name = name;
+		this.husbandName = husbandName;
+		this.sectorId = sectorId;
+		this.hhId = hhId;
+	}
+	
+}
